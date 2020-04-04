748e698376b8ce840d3896eb82ca9c6cc0df9644
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-04-04 16:20:37 +0300

    Removed unused code

--- modules/projects/main.tf
+++ modules/projects/main.tf
@@ -12,19 +12,3 @@ resource "google_project" "project-no-vpc" {
   auto_create_network = false
   billing_account     = "013FD1-9B204A-8B60C7"
 }
-
-# locals {
-#   api_set = var.enable_apis ? toset(var.activate_apis) : []
-# }
-
-/******************************************
-  APIs configuration
- *****************************************/
-# resource "google_project_service" "project_services" {
-#   for_each = local.api_set
-#   # project                    = "landing-zone-demo-false4"
-#   project                    = var.project-id
-#   service                    = each.value
-#   disable_on_destroy         = var.disable_services_on_destroy
-#   disable_dependent_services = var.disable_dependent_services
-# }
