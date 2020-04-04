b69158045efb68a14a68fc29c238d530c4049ef6
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-04-04 17:05:54 +0300

    CleanUp

--- modules/projects/main.tf
+++ modules/projects/main.tf
@@ -2,7 +2,7 @@
 #   name            = "Default Project"
 #   project_id      = var.project-id
 #   folder_id       = var.folder-id
-#   billing_account = "013FD1-9B204A-8B60C7"
+#   billing_account = var.billing_account
 # }
 
 resource "google_project" "project-no-vpc" {
