b4de6fdffc482835ec43e10fdfa85f95a71d9d84
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-06-21 22:24:43 +0300

    Updated to terraform 1.0

--- connections.tf
+++ connections.tf
@@ -1,11 +1,30 @@
 provider "google" {
-  credentials = file("security/landing-zone-demo-1c3ccf5ae919.json")
+  credentials = file("/Users/ukatsir/projects/terraform/gcp-landing-zone/security/demod-landing-zone-510c0f69f59c.json")
   region      = "us-east1"
-  version     = "~> 2.17.0"
+  # version     = "~> 2.17.0"
 }
 
+
 provider "google-beta" {
-  credentials = file("security/landing-zone-demo-1c3ccf5ae919.json")
+  credentials = file("/Users/ukatsir/projects/terraform/gcp-landing-zone/security/demod-landing-zone-510c0f69f59c.json")
   region      = "us-east1"
-  version     = "~> 3.17.0"
+  # version     = "~> 3.17.0"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "demod-terraform-state"
+    prefix = "terraform/demod-state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.5.0"
+    }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = ">= 2.0.0"
+    # }
+  }
 }
