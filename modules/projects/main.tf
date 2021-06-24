818d7f7be152072b1ae399ef2c546f2b5060c430
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-06-24 13:06:39 +0300

    Added a second project

--- modules/projects/main.tf
+++ modules/projects/main.tf
@@ -25,3 +25,16 @@ module "seed_project" {
   activate_apis               = ["compute.googleapis.com","iam.googleapis.com","sql-component.googleapis.com", "bigquery.googleapis.com"]
   # labels                      = var.project_labels
 }
+
+module "seed_project-no-vpc" {
+  source                      = "terraform-google-modules/project-factory/google"
+  version                     = "~> 10.1.1"
+  name                        = var.project-name-no-vpc
+  random_project_id           = "true"
+  disable_services_on_destroy = false
+  folder_id                   = var.folder-id-no-vpc
+  org_id                      = "910206293951"
+  billing_account             = var.billing_account
+  activate_apis               = ["compute.googleapis.com","iam.googleapis.com","sql-component.googleapis.com", "bigquery.googleapis.com"]
+  # labels                      = var.project_labels
+}
