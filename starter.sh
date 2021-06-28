124c1bdeb1a846e3a0552436376a09a28c756c72
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-06-28 19:47:30 +0300

    Using -out="plan.json" for plan and apply

--- starter.sh
+++ starter.sh
@@ -8,8 +8,10 @@ terraform fmt -recursive
 terraform validate
 
 # Run the 'plan' command and initialize the variable
-terraform plan -var-file="terraform.tfvars"
+# terraform plan -var-file="terraform.tfvars"
+terraform plan -var-file="terraform.tfvars" -out="plan.json"
 
 # Run the 'apply' command and pass in yes
 # echo "yes" | terraform apply -var-file="starter.tfvars" -var-file="./modules/gke/secrets.tfvars"
-echo "yes" | terraform apply -var-file="terraform.tfvars"
+# echo "yes" | terraform apply -var-file="terraform.tfvars"
+echo "yes" | terraform apply "plan.json"
