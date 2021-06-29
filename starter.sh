97b3269071c0b8957e476e281069383f1e4f317f
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-06-29 11:17:08 +0300

    Using -out="plan"

--- starter.sh
+++ starter.sh
@@ -9,9 +9,9 @@ terraform validate
 
 # Run the 'plan' command and initialize the variable
 # terraform plan -var-file="terraform.tfvars"
-terraform plan -var-file="terraform.tfvars" -out="plan.json"
+terraform plan -var-file="terraform.tfvars" -out="plan"
 
 # Run the 'apply' command and pass in yes
 # echo "yes" | terraform apply -var-file="starter.tfvars" -var-file="./modules/gke/secrets.tfvars"
 # echo "yes" | terraform apply -var-file="terraform.tfvars"
-echo "yes" | terraform apply "plan.json"
+echo "yes" | terraform apply "plan"
