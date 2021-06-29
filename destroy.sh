f56a08b0f9a5ca7cf4b3ad2c23cd5d8388ae5b24
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-06-29 10:46:42 +0300

    Added using -out file

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,4 @@
 # Run the 'destroy' command and pass in yes
 # echo "yes" | terraform destroy -var-file="terraform.tfvars"
-terraform apply "destroy-plan.json"
+terraform plan -destroy -out="destroy-plan" 
+terraform apply "destroy-plan"
